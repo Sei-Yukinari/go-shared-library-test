e3ce723b376a3dd509dc075d6dda2241474b9e1e
Sei-Yukinari <sei.yukinari@nextremer.com>
2022-11-17 11:11:49 +0900

    impl subtract

--- calc_test.go
+++ calc_test.go
@@ -14,13 +14,43 @@ func TestAdd(t *testing.T) {
 	}{
 		{
 			name: "aaa",
-			args: args{a: 1, b: 2},
+			args: args{
+				a: 1, b: 2,
+			},
 			want: 3,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "aaa",
+			args: args{
+				a: 2,
+				b: 1,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Subtract(tt.args.a, tt.args.b); got != tt.want {
 				t.Errorf("Add() = %v, want %v", got, tt.want)
 			}
 		})
