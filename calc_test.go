ba994847114f8bb628902ab75be5f920cfff0a84
Sei-Yukinari <sei.yukinari@nextremer.com>
2022-11-17 11:33:07 +0900

    impl multiply

--- calc_test.go
+++ calc_test.go
@@ -56,3 +56,31 @@ func TestSubtract(t *testing.T) {
 		})
 	}
 }
+
+func TestMultiply(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "aaa",
+			args: args{
+				a: 9,
+				b: 9,
+			},
+			want: 81,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Multiply(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Multiply() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
