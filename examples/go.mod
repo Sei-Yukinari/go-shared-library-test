c3051fc6c5a329f69290f6183eb8b519e3c82703
Sei-Yukinari <sei.yukinari@nextremer.com>
2022-11-17 11:30:10 +0900

    add sample

---
+++ examples/go.mod
@@ -0,0 +1,5 @@
+module example.com/calc
+
+go 1.19
+
+require github.com/Sei-Yukinari/go-shared-library-test v1.0.1
--- examples/main.go
+++ examples/main.go
@@ -1,1 +1,12 @@
-package examples
+package main
+
+import (
+	"fmt"
+
+	calc "github.com/Sei-Yukinari/go-shared-library-test"
+)
+
+func main() {
+	fmt.Println(calc.Add(1, 1))
+	fmt.Println(calc.Subtract(1, 1))
+}
